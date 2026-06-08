032ac716162df6395ce0401c2d9091083b7f5e18
Mohamed <mohamedshafkath@gmail.com>
2026-06-08 12:00:31 +0530

    added feature to execute single commands

--- shell.c
+++ shell.c
@@ -3,22 +3,27 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main()
 {
   char *prompt = NULL;
-  char *args;
+  char *args[10]; // Max arguments can be taken is ten
+  char *token;
+  
   while(1)
     {
-      prompt = readline("shshell> ");
+      int token_count = 0;
 
-      if (prompt == "")
+      for (int i = 0; i < 10; i++)
 	{
-	  printf("empty prompt\n");
-	  continue;
+	  args[i] = NULL;
 	}
       
-      else if (strcmp(prompt, "exit") == 0)
+      prompt = readline("shshell> ");
+      
+      if (strcmp(prompt, "exit") == 0)
 	{
 	  free(prompt);
 	  exit(0);
@@ -26,17 +31,45 @@ int main()
       
       else
 	{
-	  printf("%s\n", prompt);
-	  args = strtok(prompt, "-");
+	  // printf("%s\n", prompt);
+	  token = strtok(prompt, "- ");
 
-	  while (args != NULL)
+	  while (token != NULL)
 	    {
-	      printf("%s\n", args);
-	      args = strtok(NULL, "-");
+	      if (token_count < 10)
+		{
+		  args[token_count] = token;
+		  token_count++;
+		}
+		  token = strtok(NULL, "-");
+	       
+	    }
+
+	  pid_t pid = fork();
+
+	  if (pid < 0)
+	    {
+	      perror("Fork failed, exiting program");
+     	      exit(1);
+	    }
+
+	  if (pid == 0)
+	    {
+	      for (int i = 0; i < 10; i++)
+		{
+		  printf("args[%d]: %s\n", i, args[i]);
+		}
+	      execvp(args[0], args);
+	      perror("failed to execute ");
+	    }
+
+	  if (pid > 0)
+	    {
+	      wait(NULL);
 	    }
 	  continue;
 	}
-
+      
       free(prompt);
       
     }
